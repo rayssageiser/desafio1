1f0f9b54dd5da23fc61b9349fed60d1841d2da78
Rayssa Geiser gomes <113135409+rayssageiser@users.noreply.github.com>
2024-03-26 22:38:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function carregar() {
+   var msg = document.getElementById('msg')
+   var img = document.getElementById('imagem') 
+   var data = new Date()
+   var hora = data.getHours()
+   msg.innerHTML = `Agora são ${hora} horas.`
+   if (hora >= 0 && hora < 12){
+      img.src = 'fotomanha.png'
+      document.body.style.background = '#eee87d'
+      //Bom dia
+   } else if ( hora >= 12 && hora <= 18){
+      img.src = 'fototarde.png'
+      document.body.style.background = '#f5a558'
+      //Boa tarde
+   } else {
+      img.src = 'fotonoite.png'
+      document.body.style.background = '#2e394c'
+      //Boa noite
+   }
+}
